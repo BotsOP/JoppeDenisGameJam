1091accadaacd2f5069c8883ab37a95b149e5731
Denis Dvorkovyy <prutmaild@gmail.com>
2024-12-29 22:38:28 +0100

    commit

--- Assets/Components/Enemy/EnemyManager.cs
+++ Assets/Components/Enemy/EnemyManager.cs
@@ -12,17 +12,26 @@ public class EnemyManager : MonoBehaviour
     [SerializeField] private int startAmountEnemies = 100;
     [SerializeField] private Mesh mesh;
     [SerializeField] private Material material;
+    
     private RenderParams renderParams;
     private List<Matrix4x4> matrices = new List<Matrix4x4>();
     private List<Enemy> enemies = new List<Enemy>();
+    
+    private ComputeBuffer enemyDataBuffer;
+
+    private void OnDisable()
+    {
+        enemyDataBuffer?.Dispose();
+    }
 
     private void Awake()
     {
         renderParams = new RenderParams(material);
+        enemyDataBuffer = new ComputeBuffer(startAmountEnemies, sizeof(int));
         
         for (int i = 0; i < startAmountEnemies; i++)
         {
-            Vector3 pos = GetRandomPosition(10, Random.Range(0, Mathf.PI));
+            Vector3 pos = GetRandomPosition(10, Random.Range(0, Mathf.PI * 2));
             AddEnemy(pos);
         }
     }
@@ -49,6 +58,7 @@ public class EnemyManager : MonoBehaviour
     private void AddEnemy(Vector3 position)
     {
         Matrix4x4 matrix = Matrix4x4.TRS(position, Quaternion.identity, Vector3.one);
+        matrix.m23 = -1;
         matrices.Add(matrix);
         enemies.Add(new Enemy(10));
     }
